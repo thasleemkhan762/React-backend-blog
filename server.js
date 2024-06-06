22b01879dbc532a83cccb73dc1680d2ec94efa61
thasleemkhan762 <155559104+thasleemkhan762@users.noreply.github.com>
2024-06-06 15:52:06 +0530

    vercel-link

--- server.js
+++ server.js
@@ -9,7 +9,9 @@ const app = express();
 const port = process.env.PORT || 5001;
 
 
-app.use(cors());
+app.use(cors({
+    origin: 'https://react-front-end-blog.vercel.app'
+}));
 app.use(express.json());
 app.use("/api/blogs", require("./routes/blogsRoutes"));
 app.use(errorHandler);
