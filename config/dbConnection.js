d70195043142d5b073699546a30388d80b27fd1e
thasleemkhan762 <155559104+thasleemkhan762@users.noreply.github.com>
2024-06-13 12:44:19 +0530

    mongo

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -2,6 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDb = async () => {
     try {
+        mongoose.set('debug', true);
         const connect = await mongoose.connect(process.env.CONNECTION_STRING);
         console.log("Database connected: ", connect.connection.host,connect.connection.name);
     }
